4f96c9b76b5e43f30583190650e4fd204f6101e1
Karolis Kajėnas <karolis.kajenas1@gmail.com>
2017-03-21 22:36:49 +0200

    Added main factory.

--- Plukas1Client/Plukas1Client/FactoryServiceSingleton.cs
+++ Plukas1Client/Plukas1Client/FactoryServiceSingleton.cs
@@ -1,34 +1,24 @@
 ﻿namespace Plukas1Client
 {
     using FactoryStructs;
-    using System;
-    using System.Linq;
 
     public class FactoryServiceSingleton
     {
         private FactoryServiceSingleton() { }
 
         private static object _lockRoot = new object();
-        private static IProductFactory _productFactory;
-        public static IProductFactory ProductFactory
+        private static IMeatCandyAndDrinkFactory _productFactory;
+        public static IMeatCandyAndDrinkFactory ProductFactory
         {
             get
             {
                 if (_productFactory == null)
                     lock (_lockRoot)
                         if (_productFactory == null)
-                            _productFactory = new CandyAndDrinkFactory();
+                            _productFactory = new CandyDrinkAndMeatUniversalFactory();
 
                 return _productFactory;
             }
         }
-
-        public void InjectDP(string className, object[] args = null)
-        {
-            _productFactory = Type.GetType(className)
-                .GetConstructor(args?.Select(x => x?.GetType())?.ToArray())
-                .Invoke(args) as IProductFactory;
-        }
-
     }
 }
--- Plukas1Client/Plukas1Client/FactoryStructs/CandyDrinkAndMeatUniversalFactory.cs
+++ Plukas1Client/Plukas1Client/FactoryStructs/CandyDrinkAndMeatUniversalFactory.cs
@@ -1,38 +1,76 @@
-﻿using System;
-using Plukas1Client.Products;
-using System.Linq;
-using System.Collections.Generic;
-
-namespace Plukas1Client.FactoryStructs
+﻿namespace Plukas1Client.FactoryStructs
 {
-    class CandyDrinkAndMeatUniversalFactory : IProductParametrizedFactory
-    {
-        private object[] DefaultCandyParams { get; set; } = new object[] { "One", "Two" };
+    using System;
+    using Products;
+    using System.Linq;
 
-        public CandyBase CreateCandy(params object[] args)
+    public class CandyDrinkAndMeatUniversalFactory : IMeatCandyAndDrinkFactory
+    {
+        public CandyDrinkAndMeatUniversalFactory(object[] meatArgs = null, object[] candyArgs = null, object[] drinkArgs = null)
         {
-            if (args.Length != 1)
-                return ((args[0] as Type).GetConstructor(args.Skip(1).Select(x => x.GetType()).ToArray())
-                .Invoke(args.Skip(1).ToArray()) as CandyBase);
-            else
-                return ((args[0] as Type).GetConstructor(DefaultCandyParams.Select(x => x.GetType()).ToArray())
-               .Invoke(DefaultCandyParams) as CandyBase);
+            MeatDefaults = meatArgs;
+            CandyDefaults = candyArgs;
+            DrinkDefaults = drinkArgs;
         }
 
-        private object[] DefaultMeatParams { get; set; } = new object[] {  };
-        public MeatBase CreateMeat(params object[] args)
+        public object[] MeatDefaults { get; set; }
+        public object[] CandyDefaults { get; set; }
+        public object[] DrinkDefaults { get; set; }
+
+
+        // DRY, sorry! :(((
+        public CandyBase CreateCandy(params object[] args)
         {
-            if (args.Length != 1)
-                return ((args[0] as Type).GetConstructor(args.Skip(1).Select(x => x.GetType()).ToArray())
-                    .Invoke(args.Skip(1).ToArray()) as MeatBase);
-            else
-                return ((args[0] as Type).GetConstructor(DefaultMeatParams.Select(x => x.GetType()).ToArray())
-                     .Invoke(DefaultMeatParams) as MeatBase);
+            if (args == null || args.Length == 0)
+            {
+                if (CandyDefaults != null && CandyDefaults.Length > 0)
+                    return ((CandyDefaults[0] as Type).GetConstructor(CandyDefaults.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as CandyBase);
+            }
+            else if (args.Length == 1)
+            {
+                return ((args[0] as Type).GetConstructor(CandyDefaults.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as CandyBase);
+            }
+            else if (args.Length > 1)
+            {
+                return ((args[0] as Type).GetConstructor(args.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as CandyBase);
+            }
+            throw new NotImplementedException("No matches found.");
         }
 
         public DrinkBase CreateDrink(params object[] args)
         {
-            throw new NotImplementedException();
+            if (args == null || args.Length == 0)
+            {
+                if (DrinkDefaults != null && DrinkDefaults.Length > 0)
+                    return ((DrinkDefaults[0] as Type).GetConstructor(DrinkDefaults.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as DrinkBase);
+            }
+            else if (args.Length == 1)
+            {
+                return ((args[0] as Type).GetConstructor(DrinkDefaults.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as DrinkBase);
+            }
+            else if (args.Length > 1)
+            {
+                return ((args[0] as Type).GetConstructor(args.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as DrinkBase);
+            }
+            throw new NotImplementedException("No matches found.");
+        }
+
+        public MeatBase CreateMeat(params object[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                if (MeatDefaults != null && MeatDefaults.Length > 0)
+                    return ((MeatDefaults[0] as Type).GetConstructor(MeatDefaults.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as MeatBase);
+            }
+            else if (args.Length == 1)
+            {
+                return ((args[0] as Type).GetConstructor(MeatDefaults.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as MeatBase);
+            }
+            else if (args.Length > 1)
+            {
+                return ((args[0] as Type).GetConstructor(args.Skip(1).Select(x => x.GetType()).ToArray()).Invoke(args.Skip(1).ToArray()) as MeatBase);
+            }
+            throw new NotImplementedException("No matches found.");
         }
     }
 }
